43386d4d5c1cacbb959d5cd0babee7b362e95088
kiril <kiriltanev@proton.me>
2024-06-27 15:43:57 +0300

    02 solution added

--- 01_django_models_basics/exercise/main_app/models.py
+++ 01_django_models_basics/exercise/main_app/models.py
@@ -9,3 +9,11 @@ class Person(models.Model):
     )
 
     age = models.PositiveIntegerField()
+
+
+class Blog(models.Model):
+    post = models.TextField()
+
+    author = models.CharField(
+        max_length=35,
+    )
