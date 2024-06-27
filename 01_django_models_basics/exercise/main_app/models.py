d41d75645379c5ccab97e14e11de7a7581ed60ec
kiril <kiriltanev@proton.me>
2024-06-27 17:19:56 +0300

    08 solution added

--- 01_django_models_basics/exercise/main_app/models.py
+++ 01_django_models_basics/exercise/main_app/models.py
@@ -29,7 +29,7 @@ class WeatherForecast(models.Model):
 class Recipe(models.Model):
     name = models.CharField(
         max_length=100,
-        unique=True
+        unique=True,
     )
 
     description = models.TextField()
@@ -39,87 +39,130 @@ class Recipe(models.Model):
     cook_time = models.PositiveIntegerField()
 
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
     )
 
 
 class Product(models.Model):
     name = models.CharField(
-        max_length=70
+        max_length=70,
     )
 
     description = models.TextField()
 
     price = models.DecimalField(
         max_digits=10,
-        decimal_places=2
+        decimal_places=2,
     )
 
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
     )
 
 
 class UserProfile(models.Model):
     username = models.CharField(
         max_length=65,
-        unique=True
+        unique=True,
     )
 
     first_name = models.CharField(
         max_length=40,
         blank=True,
-        null=True
+        null=True,
     )
 
     last_name = models.CharField(
         max_length=40,
         blank=True,
-        null=True
+        null=True,
     )
 
     email = models.EmailField(
         unique=True,
-        default="students@softuni.bg"
+        default="students@softuni.bg",
     )
 
     bio = models.TextField(
-        max_length=120
+        max_length=120,
     )
 
     profile_image_url = models.URLField()
 
     created_at = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
     )
 
 
 class Exercise(models.Model):
     name = models.CharField(
-        max_length=100
+        max_length=100,
     )
 
     description = models.TextField()
 
     difficulty_level = models.CharField(
-        max_length=20
+        max_length=20,
     )
 
     duration_minutes = models.PositiveIntegerField()
 
     equipment = models.CharField(
-        max_length=90
+        max_length=90,
     )
 
     video_url = models.URLField(
         blank=True,
-        null=True
+        null=True,
     )
 
     calories_burned = models.PositiveIntegerField(
-        default=1
+        default=1,
     )
 
     is_favorite = models.BooleanField(
-        default=False
+        default=False,
     )
+
+
+class BookGenreChoices(models.TextChoices):
+    FICTION = "Fiction", "Fiction"
+    NON_FICTION = "Non-Fiction", "Non-Fiction"
+    SCIENCE_FICTION = "Science Fiction", "Science Fiction"
+    HORROR = "Horror", "Horror"
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=30,
+    )
+
+    author = models.CharField(
+        max_length=100,
+    )
+
+    genre = models.CharField(
+        max_length=20,
+        choices=BookGenreChoices.choices,
+    )
+
+    publication_date = models.DateField(
+        editable=False,
+        auto_now_add=True,
+    )
+
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+
+    is_available = models.BooleanField(
+        default=True,
+    )
+
+    rating = models.FloatField()
+
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
