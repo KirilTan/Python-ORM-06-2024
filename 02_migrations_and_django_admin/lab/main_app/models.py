a26211101b18c25259536b25992337ba62cec8d9
kiril <kiriltanev@proton.me>
2024-06-28 20:42:58 +0300

    02 solved

--- 02_migrations_and_django_admin/lab/main_app/models.py
+++ 02_migrations_and_django_admin/lab/main_app/models.py
@@ -9,3 +9,4 @@ class Product(models.Model):
     supplier = models.CharField(max_length=150, null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True, editable=False)
     last_edited = models.DateTimeField(auto_now=True, editable=False)
+    barcode = models.IntegerField()
