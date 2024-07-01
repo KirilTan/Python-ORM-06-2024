afc7697753a15ba9287416d96d1e723c6e5c0a90
kiril <kiriltanev@proton.me>
2024-07-02 00:42:30 +0300

    03 solution added

--- 02_migrations_and_django_admin/exercise/main_app/admin.py
+++ 02_migrations_and_django_admin/exercise/main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main_app.models import EventRegistration, Student
+from main_app.models import EventRegistration, Student, Movie
 
 
 # Register your models here.
@@ -65,3 +65,23 @@ class StudentAdmin(admin.ModelAdmin):
                 }
             ),
         )
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'director',
+        'release_year',
+        'genre',
+    )
+
+    list_filter = (
+        'release_year',
+        'genre',
+    )
+
+    search_fields = (
+        'title',
+        'director',
+    )
--- 02_migrations_and_django_admin/exercise/main_app/models.py
+++ 02_migrations_and_django_admin/exercise/main_app/models.py
@@ -52,3 +52,22 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+
+    director = models.CharField(
+        max_length=100,
+    )
+
+    release_year = models.PositiveIntegerField()
+
+    genre = models.CharField(
+        max_length=50,
+    )
+
+    def __str__(self):
+        return f'Movie "{self.title}" by {self.director}'
