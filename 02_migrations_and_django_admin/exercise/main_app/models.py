3da444b2e2ae10ffbd0edaaf5796b3d0ce80b268
kiril <kiriltanev@proton.me>
2024-07-02 13:47:22 +0300

    08 solution added

--- 02_migrations_and_django_admin/exercise/main_app/models.py
+++ 02_migrations_and_django_admin/exercise/main_app/models.py
@@ -141,4 +141,24 @@ class Person(models.Model):
 
     def __str__(self):
         return f"{self.name}: {self.age}"
-    
+
+
+class Item(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=1,
+    )
+
+    rarity = models.CharField(
+        max_length=20,
+        default='No rarity',
+    )
+
