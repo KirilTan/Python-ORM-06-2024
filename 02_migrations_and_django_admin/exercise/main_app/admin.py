764baee8f8309a2c692cc7e4efca5d108af56d78
kiril <kiriltanev@proton.me>
2024-07-02 12:43:26 +0300

    06 solution added

--- 02_migrations_and_django_admin/exercise/main_app/admin.py
+++ 02_migrations_and_django_admin/exercise/main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main_app.models import EventRegistration, Student, Movie, Supplier
+from main_app.models import EventRegistration, Student, Movie, Supplier, Course
 
 
 # Register your models here.
@@ -122,4 +122,54 @@ class SupplierAdmin(admin.ModelAdmin):
                         )
                 }
             ),
+        )
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'lecturer',
+        'price',
+        'start_date',
+    )
+
+    list_filter = (
+        'is_published',
+        'lecturer',
+    )
+
+    search_fields = (
+        'title',
+        'lecturer',
+    )
+
+    readonly_fields = (
+        'start_date',
+    )
+
+    fieldsets = \
+        (
+            (
+                'Course Information',
+                {
+                    'fields':
+                        (
+                            'title',
+                            'lecturer',
+                            'price',
+                            'start_date',
+                            'is_published',
+                        )
+                }
+            ),
+            (
+                'Description',
+                {
+                    'fields':
+                        (
+                            'description',
+                        )
+                }
+            )
         )
