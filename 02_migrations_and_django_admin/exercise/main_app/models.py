85c0d25e52f30a6164011db01278cf969dd95c45
kiril <kiriltanev@proton.me>
2024-07-02 18:16:38 +0300

    09 solution added

--- 02_migrations_and_django_admin/exercise/main_app/models.py
+++ 02_migrations_and_django_admin/exercise/main_app/models.py
@@ -162,3 +162,19 @@ class Item(models.Model):
         default='No rarity',
     )
 
+
+class Smartphone(models.Model):
+    brand = models.CharField(
+        max_length=100,
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+    )
+
+    category = models.CharField(
+        max_length=100,
+        default='No category',
+    )
