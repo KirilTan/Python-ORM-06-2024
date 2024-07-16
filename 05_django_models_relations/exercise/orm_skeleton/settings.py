eebe83e2ba94c37458c0f9cf57c956467b1cfd10
kiril <kiriltanev@proton.me>
2024-07-16 14:53:01 +0300

    logging added

--- 05_django_models_relations/exercise/orm_skeleton/settings.py
+++ 05_django_models_relations/exercise/orm_skeleton/settings.py
@@ -104,6 +104,26 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',  # Other levels CRITICAL, ERROR, WARNING, INFO, DEBUG
+    },
+    'loggers': {
+        'django.db.backends': {  # responsible for the sql logs
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
