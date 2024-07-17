7da5c27887b0a1918551453c34f21d24286a39d2
kiril <kiriltanev@proton.me>
2024-07-17 16:45:40 +0300

    01 solved

--- 06_models_inheritance_and_customization/lab/main_app/models.py
+++ 06_models_inheritance_and_customization/lab/main_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+class Animal(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    species = models.CharField(
+        max_length=100,
+    )
+
+    birth_date = models.DateField()
+
+    sound = models.CharField(
+        max_length=100,
+    )
+
+    def __str__(self):
+        text = f'{self.name} is a {self.species} born on {self.birth_date}. It makes {self.sound} sound'
+        return text
+
+
+class Mammal(Animal):
+    fur_color = models.CharField(
+        max_length=50,
+    )
+
+
+class Bird(Animal):
+    wing_span = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+    )
+
+
+class Reptile(Animal):
+    scale_type = models.CharField(
+        max_length=50,
+    )
