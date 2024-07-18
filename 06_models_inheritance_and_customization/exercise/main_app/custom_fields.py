a228b71811e11b4b9e6a37b9d9acae4b8167826c
kiril <kiriltanev@proton.me>
2024-07-18 15:33:41 +0300

    03 documentation added

--- 06_models_inheritance_and_customization/exercise/main_app/custom_fields.py
+++ 06_models_inheritance_and_customization/exercise/main_app/custom_fields.py
@@ -4,12 +4,33 @@ from django.core.exceptions import ValidationError
 
 class StudentIDField(models.PositiveIntegerField):
     def to_python(self, value: any) -> int or None:
+        """
+        Converts the input value to an integer if possible.
+
+        Args:
+            value (any): The value to be converted to an integer.
+
+        Returns:
+            int or None: The converted integer value if successful, otherwise raises a ValueError.
+        """
         try:
             return int(value)
         except ValueError:
             raise ValueError('Invalid input for student ID')
 
     def get_prep_value(self, value: int) -> int:
+        """
+        Prepares the value for database storage by ensuring it is a positive integer.
+
+        Args:
+            value (int): The value to be prepared for storage.
+
+        Returns:
+            int: The cleaned and validated integer value.
+
+        Raises:
+            ValidationError: If the value is less than or equal to zero.
+        """
         cleaned_value = self.to_python(value)
 
         if cleaned_value <= 0:
