6b6bb769af51933b1e082d8d8dcc5dabdda62d51
kiril <kiriltanev@proton.me>
2024-07-18 17:53:40 +0300

    05 solved

--- 06_models_inheritance_and_customization/exercise/main_app/models.py
+++ 06_models_inheritance_and_customization/exercise/main_app/models.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+from decimal import Decimal
+
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from main_app.custom_fields import StudentIDField, MaskedCreditCardField
 
@@ -199,3 +203,218 @@ class CreditCard(models.Model):
     )
 
     card_number = MaskedCreditCardField()
+
+
+class Hotel(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    address = models.CharField(
+        max_length=200,
+    )
+
+
+class Room(models.Model):
+    hotel = models.ForeignKey(
+        to=Hotel,
+        on_delete=models.CASCADE,
+    )
+
+    number = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    capacity = models.PositiveIntegerField()
+
+    total_guests = models.PositiveIntegerField()
+
+    price_per_night = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    def clean(self) -> None:
+        """
+        Validates the Room instance to ensure that the total number of guests does not exceed the room's capacity.
+
+        This method checks if the `total_guests` attribute is greater than the `capacity` attribute.
+        If the total number of guests exceeds the room's capacity, a `ValidationError` is raised.
+
+        Raises:
+            ValidationError: If the total number of guests exceeds the room's capacity.
+        """
+        if self.total_guests > self.capacity:
+            raise ValidationError("Total guests are more than the capacity of the room")
+
+    def save(self, *args, **kwargs) -> str:
+        """
+        Saves the Room instance to the database after performing validation checks.
+    
+        This method first calls the `clean` method to ensure that the instance is valid.
+        If the instance is valid, it then calls the parent class's `save` method to save
+        the instance to the database. Finally, it returns a success message.
+    
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+    
+        Returns:
+            str: A success message indicating that the room was created successfully.
+        """
+        self.clean()
+    
+        super().save(*args, **kwargs)
+    
+        return f"Room {self.number} created successfully"
+
+
+class BaseReservation(models.Model):
+    class Meta:
+        abstract = True
+
+    room = models.ForeignKey(
+        to=Room,
+        on_delete=models.CASCADE,
+    )
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    def reservation_period(self) -> int:
+        """
+        Calculates the duration of the reservation in days.
+
+        This method computes the number of days between the start date and the end date of the reservation.
+
+        Returns:
+            int: The number of days for the reservation period.
+        """
+        return (self.end_date - self.start_date).days
+
+    def calculate_total_cost(self) -> Decimal:
+        """
+        Calculates the total cost of the reservation.
+
+        This method computes the total cost by multiplying the reservation period (in days)
+        by the price per night of the room. The result is rounded to two decimal places.
+
+        Returns:
+            Decimal: The total cost of the reservation, rounded to two decimal places.
+        """
+        total_cost = self.reservation_period() * self.room.price_per_night
+
+        return round(total_cost, 2)
+
+    @property
+    def is_available(self) -> bool:
+        """
+        Checks if the room is available for the reservation period.
+    
+        This method queries the database to find any overlapping reservations for the same room.
+        If no overlapping reservations are found, the room is considered available.
+    
+        Returns:
+            bool: True if the room is available for the reservation period, False otherwise.
+        """
+        reservations = self.__class__.objects.filter(
+            room=self.room,
+            end_date__gte=self.start_date,
+            start_date__lte=self.end_date,
+        )
+    
+        return not reservations.exists()
+
+    def clean(self) -> None:
+        """
+        Validates the reservation instance to ensure that the start date is before the end date
+        and that the room is available for the reservation period.
+    
+        This method performs two validation checks:
+        1. Ensures that the start date is before the end date.
+        2. Checks if the room is available for the specified reservation period.
+    
+        Raises:
+            ValidationError: If the start date is not before the end date.
+            ValidationError: If the room is not available for the reservation period.
+        """
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date cannot be after or in the same end date")
+    
+        if not self.is_available:
+            raise ValidationError(f"Room {self.room.number} cannot be reserved")
+
+
+class RegularReservation(BaseReservation):
+    def save(self, *args, **kwargs) -> str:
+        """
+        Saves the RegularReservation instance to the database after performing validation checks.
+    
+        This method first calls the `clean` method to ensure that the instance is valid.
+        If the instance is valid, it then calls the parent class's `save` method to save
+        the instance to the database. Finally, it returns a success message.
+    
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+    
+        Returns:
+            str: A success message indicating that the regular reservation was created successfully.
+        """
+        super().clean()
+    
+        super().save(*args, **kwargs)
+    
+        return f"Regular reservation for room {self.room.number}"
+
+
+class SpecialReservation(BaseReservation):
+    # could avoid repetition with property
+    def save(self, *args, **kwargs) -> str:
+        """
+        Saves the SpecialReservation instance to the database after performing validation checks.
+    
+        This method first calls the `clean` method to ensure that the instance is valid.
+        If the instance is valid, it then calls the parent class's `save` method to save
+        the instance to the database. Finally, it returns a success message.
+    
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+    
+        Returns:
+            str: A success message indicating that the special reservation was created successfully.
+        """
+        super().clean()
+    
+        super().save(*args, **kwargs)
+    
+        return f"Special reservation for room {self.room.number}"
+
+    def extend_reservation(self, days: int) -> str:
+        """
+        Extends the reservation period by a specified number of days.
+
+        This method updates the end date of the reservation by adding the specified number of days.
+        It then checks if the room is available for the extended period. If the room is not available,
+        a ValidationError is raised. If the room is available, the reservation is saved with the new end date.
+
+        Args:
+            days (int): The number of days to extend the reservation.
+
+        Returns:
+            str: A success message indicating that the reservation was extended successfully.
+
+        Raises:
+            ValidationError: If the room is not available for the extended reservation period.
+        """
+        self.end_date += timedelta(days=days)
+
+        if not self.is_available:
+            raise ValidationError("Error during extending reservation")
+
+        self.save()
+
+        return f"Extended reservation for room {self.room.number} with {days} days"
