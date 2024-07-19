773cb17a2e0e3e10ddabb18ca976767a3f4d06ec
kiril <kiriltanev@proton.me>
2024-07-19 17:16:03 +0300

    05 solved

--- 07_advanced_django_model_techniques/lab/main_app/models.py
+++ 07_advanced_django_model_techniques/lab/main_app/models.py
@@ -2,6 +2,7 @@ from django.core import validators
 from django.db import models
 
 from main_app.validators import validate_menu_categories
+from main_app.mixins import ReviewMixin
 
 
 # Create your models here.
@@ -95,3 +96,38 @@ class FoodCriticRestaurantReview(RestaurantReview):
     class Meta(RestaurantReview.Meta):
         verbose_name = 'Food Critic Review'
         verbose_name_plural = 'Food Critic Reviews'
+
+
+class MenuReview(models.Model):
+    reviewer_name = models.CharField(
+        max_length=100,
+    )
+
+    menu = models.ForeignKey(
+        to=Menu,
+        on_delete=models.CASCADE,
+    )
+
+    review_content = models.TextField()
+
+    rating = models.PositiveIntegerField(
+        validators=[
+            validators.MaxValueValidator(5)
+        ]
+    )
+
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Menu Review'
+        verbose_name_plural = 'Menu Reviews'
+        unique_together = [
+            'reviewer_name',
+            'menu',
+        ]
+        indexes = [
+            models.Index
+            (
+                fields=['menu'],
+                name='main_app_menu_review_menu_id'
+            )
+        ]
