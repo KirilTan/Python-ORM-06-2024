03c8cb183a6fa0f8e6494a0031f160391df9ce47
kiril <kiriltanev@proton.me>
2024-07-19 16:05:40 +0300

    01 solved

--- 07_advanced_django_model_techniques/lab/caller.py
+++ 07_advanced_django_model_techniques/lab/caller.py
@@ -1,10 +1,12 @@
 import os
 import django
+from django.core.exceptions import ValidationError
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Restaurant
 
 # Create queries within functions
--- 07_advanced_django_model_techniques/lab/main_app/models.py
+++ 07_advanced_django_model_techniques/lab/main_app/models.py
@@ -1,3 +1,36 @@
+from django.core import validators
 from django.db import models
 
+
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=[
+            validators.MinLengthValidator(2, 'Name must be at least 2 characters long.'),
+            validators.MaxLengthValidator(100, 'Name cannot exceed 100 characters.'),
+        ]
+    )
+
+    location = models.CharField(
+        max_length=200,
+        validators=[
+            validators.MinLengthValidator(2, 'Location must be at least 2 characters long.'),
+            validators.MaxLengthValidator(200, 'Location cannot exceed 200 characters.'),
+        ]
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=[
+            validators.MinValueValidator(0.00, 'Rating must be at least 0.00.'),
+            validators.MaxValueValidator(5.00, 'Rating cannot exceed 5.00.'),
+        ]
+    )
+
