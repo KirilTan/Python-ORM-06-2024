96db2983112d07bb02e3a7e4b31c50e113c232bc
kiril <kiriltanev@proton.me>
2024-07-25 13:05:24 +0300

    user email updated

--- 09_sqlalchemy/lab/pythonProject/services.py
+++ 09_sqlalchemy/lab/pythonProject/services.py
@@ -3,6 +3,11 @@ from main import Session
 
 
 with Session() as session:
-    users = session.query(User).all()
-    for user in users:
-        print(user.username, user.email)
+    user_to_update = session.query(User).filter_by(username='john_doe').first()
+
+    if user_to_update:
+        user_to_update.email = 'new_email@example.com'
+        session.commit()
+        print('User email updated successfully.')
+    else:
+        print('User not found.')
