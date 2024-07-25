0c27fa79e514a98cb0ba49184ee6dcd795437331
kiril <kiriltanev@proton.me>
2024-07-25 13:21:36 +0300

    orders added

--- 09_sqlalchemy/lab/pythonProject/services.py
+++ 09_sqlalchemy/lab/pythonProject/services.py
@@ -1,42 +1,10 @@
 from main import Session
-from models import User
-
+from models import User, Order
 
 with Session() as session:
-    # Create new users
-    user1 = User(
-        username='john_doe',
-        email='john.doe@example.com'
+    session.add_all(
+        (
+            Order(user_id=7), Order(user_id=9)
+        )
     )
-
-    user2 = User(
-        username='sarah_smith',
-        email='sarah.smith@gmail.com'
-    )
-
-    user3 = User(
-        username='mike_jones',
-        email='mike.jones@company.com'
-    )
-
-    user4 = User(
-        username='emma_wilson',
-        email='emma.wilson@domain.net'
-    )
-
-    user5 = User(
-        username='david_brown',
-        email='david.brown@email.org'
-    )
-
-    # Add users using add_all() method
-    session.add_all([user1, user2, user3, user4, user5])
-
-    # Commit changes
     session.commit()
-
-    # Retrieve all users using all() method
-    all_users = session.query(User).all()
-
-    for user in all_users:
-        print(f'User: {user.username}, Email: {user.email}')
