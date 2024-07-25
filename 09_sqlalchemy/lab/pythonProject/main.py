569b43014ebc99c72f63bfc09c831bbc370693d3
kiril <kiriltanev@proton.me>
2024-07-25 13:31:17 +0300

    database pooling and overflow set up

--- 09_sqlalchemy/lab/pythonProject/main.py
+++ 09_sqlalchemy/lab/pythonProject/main.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 
 DATABASE_URL = 'postgresql+psycopg2://postgres:password@localhost:5432/09_lab'
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
 
 Session = sessionmaker(bind=engine)
