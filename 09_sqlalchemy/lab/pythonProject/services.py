11a8f743bd7b18cc16eb40f19e2b2e69af73c441
kiril <kiriltanev@proton.me>
2024-07-25 13:30:13 +0300

    orders print

--- 09_sqlalchemy/lab/pythonProject/services.py
+++ 09_sqlalchemy/lab/pythonProject/services.py
@@ -2,9 +2,17 @@ from main import Session
 from models import User, Order
 
 with Session() as session:
-    session.add_all(
-        (
-            Order(user_id=7), Order(user_id=9)
-        )
-    )
-    session.commit()
+    orders = session.query(Order).order_by(Order.user_id.desc()).all()
+
+    if not orders:
+        print("No orders found.")
+    else:
+        for i, order in enumerate(orders):
+            user = order.user
+            print(
+                f'Order number {order.id}\n'
+                f'Is completed: {order.is_completed}\n'
+                f'Username: {user.username}'
+            )
+            if i < len(orders) - 1:
+                print()
