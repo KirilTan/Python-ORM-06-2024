1f901508d19ef7cde15c4f547f2a5c88d517afb2
kiril <kiriltanev@proton.me>
2024-07-25 13:06:31 +0300

    user deleted

--- 09_sqlalchemy/lab/pythonProject/services.py
+++ 09_sqlalchemy/lab/pythonProject/services.py
@@ -3,11 +3,11 @@ from main import Session
 
 
 with Session() as session:
-    user_to_update = session.query(User).filter_by(username='john_doe').first()
+    user_to_delete = session.query(User).filter_by(username='john_doe').first()
 
-    if user_to_update:
-        user_to_update.email = 'new_email@example.com'
+    if user_to_delete:
+        session.delete(user_to_delete)
         session.commit()
-        print('User email updated successfully.')
+        print(f"User '{user_to_delete.username}' deleted successfully.")
     else:
         print('User not found.')
