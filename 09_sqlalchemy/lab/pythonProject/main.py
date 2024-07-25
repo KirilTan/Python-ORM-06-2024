ab1be8fbaa5e3a0e5e0cc5cd9c02ceba20fbfb19
kiril <kiriltanev@proton.me>
2024-07-25 12:58:00 +0300

    session created

--- 09_sqlalchemy/lab/pythonProject/main.py
+++ 09_sqlalchemy/lab/pythonProject/main.py
@@ -1,4 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 DATABASE_URL = 'postgresql+psycopg2://postgres:password@localhost:5432/09_lab'
 engine = create_engine(DATABASE_URL)
+
+Session = sessionmaker(bind=engine)
